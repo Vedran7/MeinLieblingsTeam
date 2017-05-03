8c2d0c845afca224e52cf5f073706f31988c497a
consenn <suender.jens@gmail.com>
2017-05-03 15:29:21 +0200

    Anzeige Eingabefehler hinzugefügt

--- js/addplayer.js
+++ js/addplayer.js
@@ -1,9 +1,93 @@
 var vornameCorrect = false;
+var nameCorrect = false;
+var jahrCorrect = false;
+var vereinCorrect = false;
+var hcoachCorrect = false;
+var acoachCorrect = false;
+var numberCorrect = false;
 
-$('#vorname').bind('input propertychange', function() {
 
-    var vorname = document.getElementById('vorname').value
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
 
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
+    if (vorname.length > 3) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    }else{
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+
+});
+
+$('#vorname').bind('input propertychange',
+function() {
+var vorname = document.getElementById('vorname').value
     if (vorname.length > 3) {
       document.getElementById('vornameOutput').innerHTML = "&#10004;"
       vornameCorrect = true;
