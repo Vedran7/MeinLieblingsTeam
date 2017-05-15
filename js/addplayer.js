9e3f3e3c22614917936fe2c0df3ead912d293484
consenn <suender.jens@gmail.com>
2017-05-15 15:13:07 +0200

    aktiv() Methode hinzugefügt; Values von position angepasst; favourit angepasst!?

--- js/addplayer.js
+++ js/addplayer.js
@@ -6,6 +6,7 @@ var hcoachCorrect = false;
 var acoachCorrect = false;
 var numberCorrect = false;
 
+
 var buchstaben = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÄÖÜßäöüß";
 
 function buchstabenPruefen(input) {
@@ -18,30 +19,30 @@ function buchstabenPruefen(input) {
 }
 
 $('#vorname').bind('input propertychange',
-function() {
-  var vorname = document.getElementById('vorname').value;
-  if (vorname.length > 0 && buchstabenPruefen(vorname)) {
-    document.getElementById('vornameOutput').innerHTML = "&#10004;"
-    vornameCorrect = true;
-  }else {
-    document.getElementById('vornameOutput').innerHTML = "&#10006;"
-    vornameCorrect = false;
-  }
-  checkAll();
-});
+  function() {
+    var vorname = document.getElementById('vorname').value;
+    if (vorname.length > 0 && buchstabenPruefen(vorname)) {
+      document.getElementById('vornameOutput').innerHTML = "&#10004;"
+      vornameCorrect = true;
+    } else {
+      document.getElementById('vornameOutput').innerHTML = "&#10006;"
+      vornameCorrect = false;
+    }
+    checkAll();
+  });
 
 $('#name').bind('input propertychange',
-function() {
-  var name = document.getElementById('name').value;
-  if (name.length > 0 && buchstabenPruefen(name)) {
-    document.getElementById('nameOutput').innerHTML = "&#10004;"
-    nameCorrect = true;
-  }else {
-    document.getElementById('nameOutput').innerHTML = "&#10006;"
-    nameCorrect = false;
-  }
-  checkAll();
-});
+  function() {
+    var name = document.getElementById('name').value;
+    if (name.length > 0 && buchstabenPruefen(name)) {
+      document.getElementById('nameOutput').innerHTML = "&#10004;"
+      nameCorrect = true;
+    } else {
+      document.getElementById('nameOutput').innerHTML = "&#10006;"
+      nameCorrect = false;
+    }
+    checkAll();
+  });
 
 $('#jahr').bind('input propertychange',
   function() {
@@ -57,43 +58,43 @@ $('#jahr').bind('input propertychange',
   });
 
 $('#verein').bind('input propertychange',
-function() {
-  var verein = document.getElementById('verein').value;
-  if (verein.length > 0 && buchstabenPruefen(verein)) {
-    document.getElementById('vereinOutput').innerHTML = "&#10004;"
-    vereinCorrect = true;
-  }else {
-    document.getElementById('vereinOutput').innerHTML = "&#10006;"
-    vereinCorrect = false;
-  }
-  checkAll();
-});
+  function() {
+    var verein = document.getElementById('verein').value;
+    if (verein.length > 0 && buchstabenPruefen(verein)) {
+      document.getElementById('vereinOutput').innerHTML = "&#10004;"
+      vereinCorrect = true;
+    } else {
+      document.getElementById('vereinOutput').innerHTML = "&#10006;"
+      vereinCorrect = false;
+    }
+    checkAll();
+  });
 
 $('#hcoach').bind('input propertychange',
-function() {
-  var hcoach = document.getElementById('hcoach').value;
-  if (hcoach.length > 0 && buchstabenPruefen(hcoach)) {
-    document.getElementById('hcoachOutput').innerHTML = "&#10004;"
-    hcoachCorrect = true;
-  }else {
-    document.getElementById('hcoachOutput').innerHTML = "&#10006;"
-    hcoachCorrect = false;
-  }
-  checkAll();
-});
+  function() {
+    var hcoach = document.getElementById('hcoach').value;
+    if (hcoach.length > 0 && buchstabenPruefen(hcoach)) {
+      document.getElementById('hcoachOutput').innerHTML = "&#10004;"
+      hcoachCorrect = true;
+    } else {
+      document.getElementById('hcoachOutput').innerHTML = "&#10006;"
+      hcoachCorrect = false;
+    }
+    checkAll();
+  });
 
 $('#acoach').bind('input propertychange',
-function() {
-  var acoach = document.getElementById('acoach').value;
-  if (acoach.length > 0 && buchstabenPruefen(acoach)) {
-    document.getElementById('acoachOutput').innerHTML = "&#10004;"
-    acoachCorrect = true;
-  }else {
-    document.getElementById('acoachOutput').innerHTML = "&#10006;"
-    acoachCorrect = false;
-  }
-  checkAll();
-});
+  function() {
+    var acoach = document.getElementById('acoach').value;
+    if (acoach.length > 0 && buchstabenPruefen(acoach)) {
+      document.getElementById('acoachOutput').innerHTML = "&#10004;"
+      acoachCorrect = true;
+    } else {
+      document.getElementById('acoachOutput').innerHTML = "&#10006;"
+      acoachCorrect = false;
+    }
+    checkAll();
+  });
 
 $('#number').bind('input propertychange',
   function() {
@@ -108,34 +109,51 @@ $('#number').bind('input propertychange',
     checkAll();
   });
 
-  function checkAll(){
-    if (vornameCorrect && nameCorrect && jahrCorrect && vereinCorrect && hcoachCorrect && acoachCorrect && numberCorrect) {
-      document.getElementById("confirmation").className = "btn btn-warning btn-block";
-    }else {
-      document.getElementById("confirmation").className = "btn btn-default btn-block disabled";
-    }
+function aktiv() {
+  var aktiv = document.getElementById('aktiv-1').checked;
+  if (aktiv) {
+    return true;
   }
+  return false;
+}
+
+function checkAll() {
+  if (vornameCorrect && nameCorrect && jahrCorrect && vereinCorrect && hcoachCorrect && acoachCorrect && numberCorrect) {
+    document.getElementById("confirmation").className = "btn btn-warning btn-block";
+  } else {
+    document.getElementById("confirmation").className = "btn btn-default btn-block disabled";
+  }
+}
 
 
 function validateForm() {
   if (vornameCorrect && nameCorrect && jahrCorrect && vereinCorrect && hcoachCorrect && acoachCorrect && numberCorrect) {
-    var vorname = document.getElementById('vorname').value
-    var name = document.getElementById('name').value
+    var vorname = document.getElementById('vorname').value;
+    var name = document.getElementById('name').value;
     var jahr = document.getElementById('jahr').value;
-    var verein = document.getElementById('verein').value
-    var hcoach = document.getElementById('hcoach').value
-    var acoach = document.getElementById('acoach').value
+    var aktiv = aktiv();
+    var position = document.getElementById('position').value;
+    var verein = document.getElementById('verein').value;
+    var hcoach = document.getElementById('hcoach').value;
+    var acoach = document.getElementById('acoach').value;
     var number = document.getElementById('number').value;
+    var favorit = document.getElementById('favorit').checked;
 
     var formData = new FormData();
 
     formData.append('vorname', vorname);
     formData.append('name', name);
     formData.append('jahr', jahr);
+    formData.append('aktiv', aktiv);
+    formData.append('position', position);
     formData.append('verein', verein);
     formData.append('hcoach', hcoach);
     formData.append('acoach', acoach);
     formData.append('number', number);
+    formData.append('favorit', favorit);
+
+
+
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://188.166.165.74:13337/api/players', true);
