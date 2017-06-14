ee851323b053093dbc8fa8b9f5fa9b6e4a4e518a
chris274 <roth.christopher8@gmail.com>
2017-06-14 13:57:49 +0200

    search 0.1

--- Server/index.js
+++ Server/index.js
@@ -6,33 +6,42 @@ app.use(cors());
 
 app.get('/api/players', function (req, res) {
    var fav = req.query.favorites;
+   var searchChar = req.query.search;
 
    var fs=require('fs');
    var data=fs.readFileSync('players.json');
    var players=JSON.parse(data);
 
-   var json = '[';
    res.header('Content-Type', 'application/json');
 
    if (typeof fav != 'undefined' && fav == 'true') {
-     for (var i = 0; i < players.length; i++) {
-       if (players[i].favorit == true) {
-         if (i!=0) {
-           json+=',';
-         }
-         json+=JSON.stringify(players[i]);
-       }
-     }
-     json+=']';
-     console.log(json);
-     res.send(JSON.parse(json));
-
+      res.send(favorit(players));
+   }else if (typeof searchChar !='undefined') {
+      res.send(search(players,searchChar));
    }else{
       res.send(players);
    }
 
 })
 
+function favorit(players,res){
+  var json = '[';
+  for (var i = 0; i < players.length; i++) {
+    if (players[i].favorit == true) {
+      if (i!=0) {
+        json+=',';
+      }
+      json+=JSON.stringify(players[i]);
+    }
+  }
+  json+=']';
+  return JSON.parse(json);
+}
+
+function search(players,searchChar){
+  
+  res.status(404).send("Oh uh, something went wrong");
+}
 
 
 
